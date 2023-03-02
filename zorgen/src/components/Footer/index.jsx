22343c0356f167c7256257d555810dc40391481b
Bruno <bruno.morales99@icloud.com>
2023-03-02 20:40:02 -0300

    last att website zorgen

--- zorgen/src/components/Footer/index.jsx
+++ zorgen/src/components/Footer/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import Logo from '../Header/components/Logo'
-import IconLinkedin from '../../assets/images/carbon_logo-linkedin.png'
-import IconInstagram from '../../assets/images/carbon_logo-instagram.png'
-import IconWpp from '../../assets/images/carbon_logo-wpp.png'
 import ListFooter from './ListFooter'
+import ListSection from './ListSection'
 
 export default function Footer() {
   return (
@@ -14,11 +12,7 @@ export default function Footer() {
       <div className='mt-7 mx-7 text-texto_final mb-5'>
         <p className='text-xs font-medium'>"Cuidar dos nossos clientes e parceiros sempre fez parte Growtech e agora faz parte da nossa nova identidade também: "Zorgen", que significa "cuidar", é o nosso novo nome, nossa nova proposta pra cuidar ainda melhor da sua empresa!</p>
       </div>
-      <section className='flex justify-center sm:justify-start mb-7'>
-        <a href="#"><img className='mx-3 sm:mx-1 sm:ml-7' src={IconLinkedin} alt="Icone do Linkedin" /></a>
-        <a href="#"><img className='mx-3 sm:mx-1' src={IconInstagram} alt="Icone do Instagram" /></a>
-        <a href="#"><img className='mx-3 sm:mx-1' src={IconWpp} alt="Icone do Whatsapp" /></a>
-      </section>
+      <ListSection />
       <div className='flex row flex-wrap mx-7'>
       <ListFooter />
         <p className='flex-1 font-normal text-xs text-titulos sm:text-center sm:mb-5 ml-12'>
